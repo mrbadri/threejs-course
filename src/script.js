9e8c4062987182db2b686bcb9a644e2a95770da6
MrBadri <badri0000badri@gmail.com>
2022-09-09 19:25:57 +0430

    FEAT: add group

--- src/script.js
+++ src/script.js
@@ -10,19 +10,47 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0.7, -0.6, 1);
-mesh.scale.set(2, 0.5, 0.5);
-mesh.rotation.y = Math.PI;
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.set(0.1, -0.5, 1);
+// mesh.scale.set(2, 0.5, 0.5);
+// mesh.rotation.reorder("XYZ");
+// mesh.rotation.set(Math.PI / 4, Math.PI / 4, 0);
+// scene.add(mesh);
+
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 /**
  * Axes Helper
  */
 const axesHelper = new THREE.AxesHelper(2);
-scene.add(axesHelper);
+// axesHelper.rotation.set(Math.PI / 4, Math.PI / 4, 0);
+group.add(axesHelper);
 
 /**
  * Sizes
@@ -39,6 +67,9 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 // camera.position.set(1,1,3);
 scene.add(camera);
+
+// camera.lookAt(new THREE.Vector3(1, 0, 0));
+// camera.lookAt(mesh.position);
 
 // console.log(mesh.position.distanceTo(camera.position));
 // mesh.position.normalize();
