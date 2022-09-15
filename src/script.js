4ce244d986d0613a5fe940f3e1970ed345564806
MrBadri <badri0000badri@gmail.com>
2022-09-15 13:02:17 +0430

    FEAT: Handle adaption frame in any device

--- src/script.js
+++ src/script.js
@@ -1,59 +1,72 @@
-import './style.css'
-import * as THREE from 'three'
-import gsap from 'gsap'
+import "./style.css";
+import * as THREE from "three";
 
-/**
- * Base
- */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-/**
- * Base
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
 
-/**
- * Animate
- */
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+let time = Date.now();
+let flag = true;
 
-const tick = () =>
-{
-    // Render
-    renderer.render(scene, camera)
+const tick = () => {
+  // get delta time
+  const currentTime = Date.now();
+  const deltaTime = currentTime - time;
+  time = currentTime;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  console.log("deltaTime:", deltaTime);
 
-tick()
+  // Update object
+  mesh.rotation.y += 0.0075;
+  mesh.rotation.z += 0.0075;
+  mesh.rotation.x += 0.0075;
+
+  // console.log("flag", flag, Math.round(mesh.position.x));
+
+  // if (mesh.position.x < 2 && flag) {
+  //   if (Math.round(mesh.position.x) == 2) {
+  //     flag = false;
+  //   }
+  //   // mesh.position.x += 0.0075;
+  //   // mesh.position.z += 0.01;
+  //   mesh.scale.z += 0.01;
+  //   mesh.scale.y -= 0.01;
+  //   mesh.scale.x += 0.02;
+  // } else {
+  //   if (Math.round(mesh.position.x) == -2) flag = true;
+  //   // mesh.position.x -= 0.0075;
+  //   // mesh.position.z -= 0.01;
+  // }
+
+  // render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
